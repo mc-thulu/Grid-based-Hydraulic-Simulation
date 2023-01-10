e153e41b80c6677af65c851016b0360b07905163
Hendrik <83767323+mc-thulu@users.noreply.github.com>
2023-01-10 09:54:36 +0100

    seperate function for fill depressions

--- src/manning.cpp
+++ src/manning.cpp
@@ -5,38 +5,7 @@
 
 namespace gbhs {
 
-void Manning::step(const float& dt) {
-    // TODO constants for now
-    float w = 0.5f;
-    float r = 0.035f;
-
-    // in- and outflow
-    for (const size_t& cell_idx : data.cellsWithWater()) {
-        Cell& c = data.getCell(cell_idx);
-
-        const auto& neighbours = c.neighbours;
-        if (neighbours.size() == 1) {  // TODO only one neighbour for now
-            // calc flow
-            float s = abs(data.cellGradient(cell_idx, neighbours[0]));
-            float h = c.water_level;
-            float l = data.cellDistance(cell_idx, neighbours[0]);
-            float outflow = (dt / (l * w)) * w * h * (1.f / r) *
-                            powf((w * h) / (w + 2.f * h), 2.f / 3.f) * sqrtf(s);
-            if (outflow > h) {
-                outflow = h;
-            }
-            c.water_level_change -= outflow;
-            data.getCell(neighbours[0]).water_level_change += outflow;
-        }
-    }
-
-    // apply in-/outflow & removing negative water levels
-    for (const size_t& cell_idx : data.cellsWithWater()) {
-        Cell& c = data.getCell(cell_idx);
-        data.setWaterLevel(cell_idx, std::max(0.f, c.water_level + c.water_level_change));
-        c.water_level_change = 0.0f;
-    }
-
+void Manning::fillDepressions() {
     // "fill_depressions"
     std::sort(data.cellsWithWater().begin(),
               data.cellsWithWater().end(),
@@ -73,12 +42,42 @@ void Manning::step(const float& dt) {
             }
         }
     }
+}
 
-    // ground water & removing negative water levels
+void Manning::step(const float& dt) {
+    // TODO constants for now
+    float w = 0.5f;
+    float r = 0.035f;
+
+    // in- and outflow
     for (const size_t& cell_idx : data.cellsWithWater()) {
         Cell& c = data.getCell(cell_idx);
-        data.setWaterLevel(cell_idx, std::max(0.f, c.water_level - 0.001f * dt));
+
+        const auto& neighbours = c.neighbours;
+        if (neighbours.size() == 1) {  // TODO only one neighbour for now
+            // calc flow
+            float s = abs(data.cellGradient(cell_idx, neighbours[0]));
+            float h = c.water_level;
+            float l = data.cellDistance(cell_idx, neighbours[0]);
+            float outflow = (dt / (l * w)) * w * h * (1.f / r) *
+                            powf((w * h) / (w + 2.f * h), 2.f / 3.f) * sqrtf(s);
+            if (outflow > h) {
+                outflow = h;
+            }
+            c.water_level_change -= outflow;
+            data.getCell(neighbours[0]).water_level_change += outflow;
+        }
     }
+
+    // apply in-/outflow & removing negative water levels
+    for (const size_t& cell_idx : data.cellsWithWater()) {
+        Cell& c = data.getCell(cell_idx);
+        data.setWaterLevel(
+            cell_idx, std::max(0.f, c.water_level + c.water_level_change - 0.001f * dt));
+        c.water_level_change = 0.0f;
+    }
+
+    fillDepressions();
 }
 
 }  // namespace gbhs
--- src/manning.hpp
+++ src/manning.hpp
@@ -14,6 +14,7 @@ class Manning {
 
    private:
     SimulationData& data;
+    void fillDepressions();
 };
 
 }  // namespace gbhs
